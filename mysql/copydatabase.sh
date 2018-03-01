ab27e8a976ad5b3b8b20458c9a1407770653c836
Joe <basamajoe@gmail.com>
2018-03-01 18:20:01 +0100

    Nomencalture and comment updatels

--- mysql/copydatabase.sh
+++ mysql/copydatabase.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-DBADA_PRE=serverpre
-DBADA_DEV=serverdev
+HOST_ORIGIN=serverori
+HOST_DESTINATION=serverdes
 DBNAME=dbname
 
+#It uses same user for both servers
 read -p "user: " user
 read -s -p "pass: " pass
 
 # Export
-mysqldump -u $user -p$pass -h $DBADA_PRE --databases $DBNAME > $DBNAME.sql
+mysqldump -u $user -p$pass -h $HOST_ORIGIN --databases $DBNAME > $DBNAME.sql
 
 # Drop database ada
-#mysqladmin -u$user -p$pass -h $DBADA_DEV drop $DBNAME
-mysql -u$user -p$pass -h $DBADA_DEV -e "DROP DATABASE IF EXISTS $DBNAME"
+#mysqladmin -u$user -p$pass -h $HOST_DESTINATION drop $DBNAME
+mysql -u$user -p$pass -h $HOST_DESTINATION -e "DROP DATABASE IF EXISTS $DBNAME"
 
 #Create database adapters
-#mysqladmin -u$user -p$pass -h $DBADA_DEV create databasename $DBNAME
-mysql -u$user -p$pass -h $DBADA_DEV -e "CREATE DATABASE IF NOT EXISTS $DBNAME"
+#mysqladmin -u$user -p$pass -h $HOST_DESTINATION create databasename $DBNAME
+mysql -u$user -p$pass -h $HOST_DESTINATION -e "CREATE DATABASE IF NOT EXISTS $DBNAME"
 
 # Import
-mysql -u$user -p$pass -h $DBADA_DEV --debug-info --progress-reports $DBNAME < $DBNAME.sql
+mysql -u$user -p$pass -h $HOST_DESTINATION --debug-info --progress-reports $DBNAME < $DBNAME.sql
